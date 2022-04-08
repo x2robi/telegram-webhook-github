e6cfc9abf28905e578e8962644bfbfaac0121397
xshady <54737754+xxshady@users.noreply.github.com>
2022-04-08 03:13:45 +0300

    replace pusher name with username

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ app.use(bodyParser.json());
 
 app.post("/", async (req, res) => {
     const data = req.body;
-    const sender = data.pusher.name;
+    const sender = data.pusher.username;
     const branch = data.repository.master_branch;
     const commit = data.head_commit.message;
     const commit_url = data.head_commit.url;
     const commits = data.commits.length;
 
-    bot.sendMessage(chatId, `💬 *${sender}* push ${branch}\n[${commit} (${commits} commits)](${commit_url})`, {
+    bot.sendMessage(chatId, `💬 *${username}* push ${branch}\n[${commit} (${commits} commits)](${commit_url})`, {
         parse_mode: 'Markdown'
     })
 
